7194ef571b184bfd9606b0a143d6491fb7693e71
Lucas Cavalheiro <lucasribeiro120@hotmail.com>
2022-02-25 18:52:44 -0300

    Added files to heroku

--- data/connection.js
+++ data/connection.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 
 function makeConnectionWithDatabase() {
     mongoose.connect(process.env.MONGODB_CONNECTION, (err) => {
-
         if (err) throw err;
         console.log("Database connected");
     })
--- routes/Expense/expense-route.js
+++ routes/Expense/expense-route.js
@@ -3,43 +3,33 @@ const routerExpense = express.Router();
 const ExpenseModel = require("../../data/modules/Expense");
 
 routerExpense.get("/", async(req, res) => {
-
     try{
         const expense = await ExpenseModel.find();
-
         res.status(200).json(expense);
     }
     catch(err){
-
         res.status(400).json({message: err});
     }
-
 })
 
 routerExpense.get("/total", async(req,res) => {
-
     try {
         const expese = await ExpenseModel.find({}, {_id:0,value:1})
         let total = 0;
         expese.forEach( expeseItem => {
-            
             total += expeseItem.value
         })
         res.status(200).json(total);
-
     } catch (error) {
         res.status(400).json({message: error});
     }
 })
 
 routerExpense.get("/:idExpense", async(req,res) => {
-
     try {
-        
         const expense = await ExpenseModel.findById(req.params.idExpense);
         console.log(expense);
         res.status(200).json(expense);
-
     } catch (error) {
         res.status(400).json({message: error})
     }
@@ -53,13 +43,10 @@ routerExpense.post("/", async(req,res) => {
             value: req.body.value,
             date: req.body.date
         })
-
         await expense.save();
         res.status(200).json(expense);
-
     }
     catch(err){
-        
         res.status(400).json({message: err})
     }
 })
